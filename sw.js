11d0e2ae7fcff149afc0a1f5e125d6b2fbd356a7
krausioachim-lang <krausioachim@gmail.com>
2026-04-27 16:38:45 +0200

    Update service worker to version v5 with new logos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'radio-nt-v4'; // Am crescut versiunea la v2 deoarece am adăugat logo-uri noi
+const CACHE_NAME = 'radio-nt-v5'; // Am crescut versiunea la v2 deoarece am adăugat logo-uri noi
 const ASSETS = [
   './',
   './index.html',
